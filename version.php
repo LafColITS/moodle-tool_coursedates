957f5924d2b4d68547aae9df0fdb6d68734d2e00
Charles Fulton <fultonc@lafayette.edu>
2018-06-22 12:16:28 -0400

    Set course dates v3.3.1

--- version.php
+++ version.php
@@ -22,8 +22,8 @@
 
  defined('MOODLE_INTERNAL') || die();
 
- $plugin->version   = 2017082501;
+ $plugin->version   = 2017082502;
  $plugin->requires  = 2017051500;
  $plugin->component = 'tool_coursedates';
  $plugin->maturity  = MATURITY_STABLE;
- $plugin->release   = 'v3.3.0';
+ $plugin->release   = 'v3.3.1';
