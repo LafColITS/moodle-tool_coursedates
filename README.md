55d18b311c0d5416b467bcaf0cd79177e271580f
Charles Fulton <fultonc@lafayette.edu>
2021-05-18 12:39:09 -0400

    Drop support for Moodle 3.7-3.8

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ This admin tool allows managers to set the start and end dates for all courses i
 
 Requirements
 ------------
-- Moodle 3.7 (build 2019052000 or later)
+- Moodle 3.9 (build 2020061500 or later)
 
 Installation
 ------------
--- version.php
+++ version.php
@@ -25,7 +25,7 @@
  defined('MOODLE_INTERNAL') || die();
 
  $plugin->version   = 2020081400;
- $plugin->requires  = 2019052000;
+ $plugin->requires  = 2020061500;
  $plugin->component = 'tool_coursedates';
  $plugin->maturity  = MATURITY_STABLE;
  $plugin->release   = 'v3.7.0';
